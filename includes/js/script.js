67db1b455e176ffb950d7f706d7ffc378e5387c4
Moffat <ngigemoffat@gmail.com>
2018-01-23 03:59:39 +0300

    FEATURE/ Added the landing page and styling sheets for app

---
+++ includes/js/script.js
@@ -0,0 +1,31 @@
+/*
+
+My Custom JS
+============
+
+Author:  Moffat Ngige
+Updated: Jan 2018
+Credits: Tutorial from Youtube.com on JS function overwriting 
+
+*/
+
+$(function() {
+	
+	$('#alertMe').click(function(e) {
+		
+		e.preventDefault();
+		
+		$('#successAlert').slideDown();
+		
+	});
+	
+	$('a.pop').click(function(e) {
+		e.preventDefault();
+	});
+	
+	$('a.pop').popover();
+	
+	$('[rel="tooltip"]').tooltip();
+	
+	
+});
